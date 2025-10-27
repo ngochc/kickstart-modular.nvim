c46d70e1d60e47642d20d550e8c9b9c5955b87f3
Ngoc Hoang <ngoc.hc91@gmail.com>
2025-10-27 15:49:44 +0700

    Update config

--- lua/kickstart/plugins/which-key.lua
+++ lua/kickstart/plugins/which-key.lua
@@ -59,7 +59,7 @@ return {
 
       -- Document existing key chains
       spec = {
-        { '<leader>s', group = '[S]earch' },
+        { '<leader>F', group = '[F]ind' },
         { '<leader>t', group = '[T]oggle' },
         { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
       },
